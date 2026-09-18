8354824fc66b2bf304eb54881c9ff96c1e31d4b8
PythonicDG <dipakgaikwadmg@gmail.com>
2026-09-18 13:54:54 +0530

    feat: bootstrap React application

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import { AuthProvider } from './context/AuthContext';
+import { AlertProvider } from './context/AlertContext';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <AlertProvider>
+        <App />
+      </AlertProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
